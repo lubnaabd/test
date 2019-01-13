5f5b933e56e370d7537ae476a88b09590b2be30c
lubnaabad <lubna_mosa@hotmail.com>
2019-01-13 16:25:46 +0200

    upload file structre

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit
+            <code>src/App.js</code>
+            and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
