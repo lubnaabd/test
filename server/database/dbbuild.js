a5e1724ce1b4f288826c5fefcb91261cc96985e1
lubnaabad <lubna_mosa@hotmail.com>
2019-01-14 14:14:30 +0200

    call db_connection database

--- server/database/dbbuild.js
+++ server/database/dbbuild.js
@@ -1,25 +1,28 @@
 const Item = require('./models/items');
 
-
 const dbConnection = require('./db_connection');
 
 dbConnection();
 
 const buildDb = async () => {
   await Item.deleteMany({});
-
+  console.log('Item deleleted');
+  
   await Item.insertMany([
     {
       title: 'lubna',
-      discripation: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      discripation:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
       title: 'sundus',
-      discripation: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      discripation:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
       title: 'osama',
-      discripation: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      discripation:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
   ]);
   console.log('Item added');
