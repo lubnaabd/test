b48e9a1127cdd86369bef0ef8b22f55d67bb73a5
lubnaabad <lubna_mosa@hotmail.com>
2019-01-14 20:47:45 +0200

    delete dbbuild()

--- server/database/dbbuild.js
+++ server/database/dbbuild.js
@@ -5,29 +5,30 @@ const dbConnection = require('./db_connection');
 dbConnection();
 
 const buildDb = async () => {
-  await Item.deleteMany({});
-  console.log('Item deleleted');
-  
-  await Item.insertMany([
-    {
-      title: 'lubna',
-      discripation:
+  try {
+    await Item.deleteMany({});
+    console.log('Item deleleted');
+
+    await Item.insertMany([
+      {
+        title: 'lubna',
+        discripation:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-    {
-      title: 'sundus',
-      discripation:
+      },
+      {
+        title: 'sundus',
+        discripation:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-    {
-      title: 'osama',
-      discripation:
+      },
+      {
+        title: 'osama',
+        discripation:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    },
-  ]);
-  console.log('Item added');
+      },
+    ]);
+    console.log('Item added');
+  } catch (err) { console.error(err); }
 };
 
-buildDb().catch(err => console.error(err));
 
 module.exports = buildDb;
