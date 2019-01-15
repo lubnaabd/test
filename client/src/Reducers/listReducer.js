e031dfe4b317ab0fca3cb87a7a757ce00eb2f552
lubnaabad <lubna_mosa@hotmail.com>
2019-01-15 07:47:43 +0200

    put new item in list by unshift

--- client/src/Reducers/listReducer.js
+++ client/src/Reducers/listReducer.js
@@ -17,6 +17,7 @@ export default function (state = initialState, action) {
       return {
         ...state,
         item: payload,
+        ...state.items.unshift(payload),
       };
 
     case DELETE_LIST:
